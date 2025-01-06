b8eb65d81fdcddaafc2ae762a5771e7e68b6ad8f
samuel maveke <mavekepeter2022@gmail.com>
2025-01-06 16:59:36 +0300

    complete services section

--- assets/assets.js
+++ assets/assets.js
@@ -109,7 +109,7 @@ export const serviceData = [
 export const infoList = [
     { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages', description: 'HTML, CSS, JavaScript, React Js, Next Js, Python' },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'B.Tech in Business Info Technology' },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects' }
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 50 projects' }
 ];
 
 export const toolsData = [
--- components/About.jsx
+++ components/About.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 import Image from 'next/image'
 import { assets, infoList, toolsData } from '@/assets/assets'
